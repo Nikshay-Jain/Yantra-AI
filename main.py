fc14078b54dcf643d450e7974c026e3cec04df3f
Nikshay Jain <nikshay.p.jain@gmail.com>
2023-05-21 16:32:15 +0530

    Modified main.py. Songs and csv for websites added. Shut down feature too.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 import webbrowser
 import pyttsx3
+import numpy as np
+import pandas as pd
 import speech_recognition as sr
 
 # Create an instance of the Text-to-Speech engine
@@ -36,25 +38,51 @@ def hear():
             print(heard, str(e))
 
         return heard
-    
+
+sites = pd.read_csv('sites.csv')
+sites.loc[0,'name'] = "google"
+sites.loc[0,'web'] = "https://google.com/"
+sites.loc[1,'name'] = "youtube"
+sites.loc[1,'web'] = "https://youtube.com/"
+sites.loc[2,'name'] = "wikipedia"
+sites.loc[2,'web'] = "https://wikipedia.com/"
+sites.loc[3,'name'] = "chat gpt"
+sites.loc[3,'web'] = "https://chat.openai.com/"
+sites.loc[4,'name'] = "moodle"
+sites.loc[4,'web'] = "https://coursesnew.iitm.ac.in/login/index.php"
+sites.sort_values('name', inplace=True)
+sites.reset_index(drop=True , inplace=True)
+
 if __name__ == "__main__":
     print('VS Code')
     text = "Nuhmuhstay! Yantruh A.I. is at your command sir!!!"
     say(text)
-    sites = [["google","https://google.com"],["youtube","https://youtube.com"],["wikipedia","https://wikipedia.com"],["chat gpt","https://chat.openai.com/"]]
+    #sites = [["google","https://google.com"],["youtube","https://youtube.com"],["wikipedia","https://wikipedia.com"],["chat gpt","https://chat.openai.com/"]]
     while True:
         print("At your Command Sir...")
         cmd = hear()
         if "open" in cmd.lower():
-            for site in sites:
-                if f"Open {site[0]}".lower() in cmd.lower():
-                    say(f"Opening {site[0]} Sir...")
-                    webbrowser.open(site[1])
+            for site in sites['name']:
+                if f"Open {site}".lower() in cmd.lower():
+                    say(f"Opening {site} sir...")
+                    webbrowser.open(((sites.loc[sites['name']==site]).to_numpy())[0,1])
 
             #Work on webistes by webdriver package
-            if f"Open Music".lower() in cmd.lower():
-                cwd = os.getcwd()
-                file_path = os.path.join(cwd, "Atrangi Yaari.mp3")
-                webbrowser.open(file_path)
+            
+        if "play" in cmd.lower():
+            contents = os.listdir(r"C:\Users\Nikshay Jain\OneDrive - smail.iitm.ac.in\Music")
+            songs = [os.path.splitext(item)[0] for item in contents]
+            for music in songs:
+                if music.lower() in cmd.lower():
+                    dir = r'C:\Users\Nikshay Jain\OneDrive - smail.iitm.ac.in\Music'
+                    file_path = os.path.join(dir, f"{music}.mp3")
+                    webbrowser.open(file_path)
+                    break
+        
+        elif "shutdown yantra" in cmd.lower():
+            print("Yantra Shutting down...")
+            say("Yantra Shutting down")
+            break
+        
         else:
             say(cmd)
