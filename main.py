683cb10b03434a9e6d5c7e6bca55b1844476e269
Nikshay Jain <nikshay.p.jain@gmail.com>
2024-06-08 11:47:51 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ import pandas as pd
 import numpy as np
 import api_func
 import speech_recognition as sr
+import spacy
 
 # Create an instance of the Text-to-Speech engine
 engine = pyttsx3.init()
 
 # Set the properties of the speech output
-engine.setProperty("rate", 225)    # Speech speed (words per minute)
+engine.setProperty("rate", 200)    # Speech speed (words per minute)
 engine.setProperty("voice", "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0")
+
+nlp = spacy.load('en_core_web_sm')
 
 def say(text):
     engine.say(text)
@@ -22,7 +25,6 @@ def say(text):
 
 #initialise the microphone
 r = sr.Recognizer()
-#mic = sr.Microphone(device_index=0)
 
 # Adjust the energy threshold dynamically based on the ambient noise level
 with sr.Microphone() as source:
@@ -52,7 +54,7 @@ def hear():
             print(heard)
 
         except sr.RequestError as e:
-            heard = "Speech recognition request error:"
+            heard = "Sorry, my speech service is down."
             print(heard, str(e))
             
         return heard
@@ -62,71 +64,168 @@ sites.sort_values('name', inplace=True)
 sites.reset_index(drop=True , inplace=True)
 sites.to_csv("sites.csv",index=False)
 
-text = "Nuhmuhstay! Yantruh is at your command sir!!!"
-print("Namaste! Yantra is at your command sir!!!")
-say(text)
-while True:
-    f=0
-    print("At your Command Sir...")
-    cmd = hear()
-    
-    if "shutdown" in cmd.lower() :
-        print("Yantra Shutting down...")
-        say("Yantra Shutting down")
-        break
+def shutdown():
+    print("Yantra Shutting down...")
+    say("Yantra Shutting down")
+    exit()
+
+def greet():
+    say("Hello! How are you?")
+
+def introduce():
+    say("I am Yantra, your desktop assistant.")
+
+def handle_user_name():
+    say("I don't know your name yet. Can you please tell me your name?")
+    user_name = input()
+    if user_name:
+        say(f"Nice to meet you, {user_name}!")
+        return user_name
+    return None
+
+def open_website_or_app(cmd):
+    f = 0
+    for site in sites['name']:
+        if f"open {site}".lower() in cmd:
+            say(f"Opening {site}...")
+            url = sites.loc[sites['name'] == site, 'web'].values[0]
+            if url[0] == 'C':
+                subprocess.Popen(url)
+            else:
+                webbrowser.open(url)
+            f = 1
+    # if f == 0:
+    #     say("Sorry, the app or site you mentioned is not in my database. Please enter its name and URL below:")
+    #     name = input("Enter name of site below\n")
+    #     if name.lower() == 'q':
+    #         return
+    #     web = input("Enter its URL below\n")
+    #     sites.loc[len(sites)] = [name, web]
+
+def add_site_to_database(name, url):
+    """Add a new site to the database."""
+    sites.loc[len(sites)] = [name, url]
+
+def play_music(cmd):
+    music_dir = r"C:\Users\Nikshay Jain\OneDrive - smail.iitm.ac.in\Music"
+    contents = os.listdir(music_dir)
+    songs = [os.path.splitext(item)[0] for item in contents]
+    for music in songs:
+        if music.lower() in cmd:
+            file_path = os.path.join(music_dir, f"{music}.mp3")
+            webbrowser.open(file_path)
+            break
+
+def get_weather():
+    say("Please tell me the name of the city you want to know the weather of:")
+    city = input()
+    if city:
+        # Use an appropriate function to get the weather for the city
+        weather_info = api_func.weather(city)
+        say(weather_info)
+
+def get_news():
+    say("Please tell me specifically the topic you want the news about:")
+    topic = input()
+    if topic:
+        # Use an appropriate function to get the news for the topic
+        news_info = api_func.news(topic)
+        say(news_info)
+
+def tell_time():
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    print(f"The time currently is {current_time}")
+    say(f"The time currently is {current_time}")
+
+def tell_date():
+    current_date = datetime.date.today().strftime("%d/%m/%Y")
+    print(f"The date today is {current_date}")
+    date_say = datetime.date.today().strftime("%B %d, %Y")
+    say(f"The date today is {date_say}")
+
+def parse_command(cmd):
+    """Parse the command using spaCy."""
+    doc = nlp(cmd)
+    return doc
+
+def main():
+    text = "Nuhmuhstay! Yantruh is at your command!!!"
+    print("Namaste! Yantra is at your command!!!")
+    say(text)
+    user_name = ""
+    while True:
+        print("At your Command...")
+        cmd = input().lower()
+
+        doc = parse_command(cmd)
+
+        # Shutdown command
+        if any(token.lemma_ in ["shutdown", "exit", "quit"] for token in doc):
+            shutdown()
         
-    elif "open" in cmd.lower():
-        for site in sites['name']:
-            if f"Open {site}".lower() in cmd.lower():
-                say(f"Opening {site} sir...")
-                url = sites.loc[sites['name']==site].to_numpy()[0,1]
-                if url[0] == 'C':
-                    subprocess.Popen(url)
-                else:
-                    webbrowser.open(url)
-                f=1
-        if f==0:
-            say("Sorry, the app or site you mentioned is not in my database, please enter its name and its url below:")
-            name = input("Enter name of site below\n")
-            if name == 'q':
-                continue
-            web = input("Enter its url below\n")
-            sites.loc[sites.last_valid_index(),'name'] = name
-            sites.loc[sites.last_valid_index(),'web'] = web
-        f=0
+        # Greeting command
+        elif "hello" in cmd:
+            greet()
         
-    elif "play" in cmd.lower():
-        contents = os.listdir(r"C:\Users\Nikshay Jain\OneDrive - smail.iitm.ac.in\Music")
-        songs = [os.path.splitext(item)[0] for item in contents]
-        for music in songs:
-            if music.lower() in cmd.lower():
-                dir = r'C:\Users\Nikshay Jain\OneDrive - smail.iitm.ac.in\Music'
-                file_path = os.path.join(dir, f"{music}.mp3")
-                webbrowser.open(file_path)
-                break
-
-    elif "weather" in cmd.lower():
-        print("Please tell me the name of the city you want to know the weather of:")
-        say("Please tell me the name of the city you want to know the weather of:")
-        inp = hear()
-        say(api_func.weather(inp))
+        # Introduction command
+        elif "your name" in cmd:
+            introduce()
         
-    elif "news" in cmd.lower():
-        print("Please tell me specifically the topic you want the news about:")
-        say("Please tell me specifically the topic you want the news about:")
-        inp = hear()
-        say(api_func.news(inp))
+        # User name command
+        elif "my name" in cmd:
+            if user_name:
+                say(f"Your name is {user_name}.")
+            else:
+                user_name = handle_user_name()
+        
+        # Open website or app command
+        elif any(token.lemma_ in ["open"] for token in doc):
+            for token in doc:
+                if token.lemma_ == "open":
+                    site_name = token.head.text
+                    if site_name in sites['name'].values:
+                        open_website_or_app(site_name)
+                    else:
+                        say("Sorry, the app or site you mentioned is not in my database. Please enter its name and URL below:")
+                        name = input("Enter name of site below\n")
+                        if name.lower() == 'q':
+                            break
+                        url = input("Enter its URL below\n")
+                        add_site_to_database(name, url)
+                    break
+        
+        # Play music command
+        elif any(token.lemma_ in ["play"] for token in doc):
+            for token in doc:
+                if token.lemma_ == "play":
+                    song_name = token.head.text
+                    play_music(song_name)
+                    break
+        
+        # Weather command
+        elif any(token.lemma_ in ["weather"] for token in doc):
+            say("Please tell me the name of the city you want to know the weather of:")
+            city = hear()
+            if city:
+                get_weather(city)
+        
+        # News command
+        elif any(token.lemma_ in ["news"] for token in doc):
+            say("Please tell me specifically the topic you want the news about:")
+            topic = hear()
+            if topic:
+                get_news(topic)
+        
+        # Time command
+        elif any(token.lemma_ in ["time"] for token in doc):
+            tell_time()
+        
+        # Date command
+        elif any(token.lemma_ in ["date"] for token in doc):
+            tell_date()
+        
+        else:
+            say("I am sorry, I cannot do that yet.")
 
-    elif "the time" in cmd.lower():
-        time = datetime.datetime.now().strftime("%H:%M:%S")
-        print(f"The time currently is {time}")
-        say(f"Sir, the time currently is {time}")
-
-    elif "today's date" in cmd.lower() or "date today" in cmd.lower():
-        date = datetime.date.today().strftime("%d/%m/%Y")
-        print(f"The date today is {date}")
-        date_say = datetime.date.today().strftime("%B %d, %Y")
-        say(f"The date today is {date_say}")
-
-    else:
-        say(cmd)
+if __name__ == "__main__":
+    main()
